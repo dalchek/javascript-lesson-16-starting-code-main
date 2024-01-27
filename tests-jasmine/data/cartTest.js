3337b0502917b0e1004c79963c6c66fb82337c1b
Dalchek <dalchek999@gmail.com>
2024-01-27 21:38:08 +0100

    Create automated tests for orderSummary

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -27,6 +27,8 @@ describe('test suite: addToCart', () => {
         });
         loadFromStorage();
 
+        
+
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6');
         expect(cart.length).toEqual(1)
         expect(localStorage.setItem).toHaveBeenCalledTimes(1);
