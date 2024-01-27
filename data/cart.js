38da9c04850aa83cb9384dc0c03bab7e1eab89b7
Dalchek <dalchek999@gmail.com>
2024-01-27 15:47:45 +0100

    Create automated tests fro addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) {
   cart = [{
@@ -10,6 +15,7 @@ if (!cart) {
     quantity: 1,
     deliveryOptionId: '2'
   }];
+}
 }
 
 function saveToStorage() {
@@ -31,7 +37,7 @@ export function addToCart(productId) {
     cart.push({
       productId: productId,
       quantity: 1,
-      // deliveryOptionId: '1'
+      deliveryOptionId: '1'
     });
   }
 
